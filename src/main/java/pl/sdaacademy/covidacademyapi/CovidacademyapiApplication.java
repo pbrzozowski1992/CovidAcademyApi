aa6b755ccaac37f0281093055fa21cd570ed4683
Piotr Brzozowski <piotr.brzozowski.dev@gmail.com>
2020-12-12 11:02:52 +0100

    init project

---
+++ src/main/java/pl/sdaacademy/covidacademyapi/CovidacademyapiApplication.java
@@ -0,0 +1,13 @@
+package pl.sdaacademy.covidacademyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidacademyapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovidacademyapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/sdaacademy/covidacademyapi/CovidacademyapiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sdaacademy.covidacademyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidacademyapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
