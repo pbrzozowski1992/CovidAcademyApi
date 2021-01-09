2c559ae0c75691522e51e10170810b139582db5e
Piotr Brzozowski <piotr.brzozowski.dev@gmail.com>
2021-01-09 10:36:35 +0100

    feature(caa): map state name to acronym

--- src/main/java/pl/sdaacademy/covidacademyapi/states_stats/serivce/StatesStatsService.java
+++ src/main/java/pl/sdaacademy/covidacademyapi/states_stats/serivce/StatesStatsService.java
@@ -2,17 +2,23 @@ package pl.sdaacademy.covidacademyapi.states_stats.serivce;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pl.sdaacademy.covidacademyapi.states_metadata.repository.StatesMetadata;
+import pl.sdaacademy.covidacademyapi.states_metadata.serivce.StatesMetadataService;
 import pl.sdaacademy.covidacademyapi.states_stats.repository.CovidTrackingApi;
 import pl.sdaacademy.covidacademyapi.states_stats.repository.StateStats;
+
+import java.util.List;
 
 @Service
 public class StatesStatsService {
 
     private final CovidTrackingApi covidTrackingApi;
+    private final StatesMetadataService statesMetadataService;
 
     @Autowired
-    public StatesStatsService(CovidTrackingApi covidTrackingApi) {
+    public StatesStatsService(StatesMetadataService statesMetadataService, CovidTrackingApi covidTrackingApi) {
         this.covidTrackingApi = covidTrackingApi;
+        this.statesMetadataService = statesMetadataService;
     }
 
     public StateStats[] getAllStatesCurrentStats() {
@@ -21,6 +27,17 @@ public class StatesStatsService {
     }
 
     public StateStats getStatsForState(String state, String date) {
-        return covidTrackingApi.getStatsForState(state, date);
+        List<StatesMetadata> statesMetadata = statesMetadataService.getStatesMetadata();
+        String acronym = statesMetadata.stream()
+                .filter(metadata -> metadata.getName().equalsIgnoreCase(state))
+                .map(metadata->metadata.getState())
+                .findAny().get();
+
+        //mapowanie nazwy stanu na akronim
+        //zampowania wartość powinna zostać przekazana do covidTrackingApi.getStatsForState
+        //musimy wykorzystać StatesMetadataService w celu pobrania metadanych dla każdego stanu
+        //musimy znaleść na liście metadanych konkretny stan po nazwie
+        //jeśli stan istnieje powiniśmy pobrać akronim z obiektu
+        return covidTrackingApi.getStatsForState(acronym, date);
     }
 }
